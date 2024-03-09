c8f1c2f889b2fcb0fbe67f59132eba74ad68c471
skri0010 <skri0010@student.monash.edu>
2024-03-09 22:31:24 +0530

    added django app models for Car and Car make/updated views

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -1,5 +1,5 @@
-# from django.contrib import admin
-# from .models import related models
+from django.contrib import admin
+from .models import CarMake, CarModel
 
 
 # Register your models here.
@@ -7,7 +7,8 @@
 # CarModelInline class
 
 # CarModelAdmin class
+admin.site.register(CarModel)
 
 # CarMakeAdmin class with CarModelInline
-
+admin.site.register(CarMake)
 # Register models here
